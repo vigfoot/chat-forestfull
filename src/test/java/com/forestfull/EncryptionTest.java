ec54a8e562783241875fbf052dfa3e9113807593
Xasquatch <bigfoot7774@gmail.com>
2025-11-25 15:40:09 +0900

    라이브러리 연동

---
+++ src/test/java/com/forestfull/EncryptionTest.java
@@ -0,0 +1,22 @@
+package com.forestfull;
+
+import lombok.RequiredArgsConstructor;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
+class EncryptionTest {
+
+    @Autowired
+    private StringEncryptor encryptor;
+
+    @Test
+    void encrypt() {
+        String encrypted = encryptor.encrypt("blarblar");
+        System.out.println("Encrypted: " + encrypted);
+    }
+}
