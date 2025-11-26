ab1c66c3e067a9142be9583eb5e6da47c867f43a
Xasquatch <bigfoot7774@gmail.com>
2025-11-27 04:32:53 +0900

    -

--- src/main/java/com/forestfull/chat/ChatRoomMapper.java
+++ src/main/java/com/forestfull/chat/ChatRoomMapper.java
@@ -4,14 +4,9 @@ import com.forestfull.member.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-/**
- * com.forestfull.chat
- *
- * @author vigfoot
- * @version 2025-11-27
- */
 @Mapper
 public interface ChatRoomMapper {
     void joinRoom(MemberDTO.Room memberRoom);
+
     void leaveRoom(@Param("roomId") Long roomId, @Param("memberId") Long memberId);
 }
