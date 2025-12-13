85426ec29d4009d4a3debf5273e47b73a9af6bec
Xasquatch <bigfoot7774@gmail.com>
2025-12-13 19:47:05 +0900

    마이바티스 재연동 (트랜잭션 관리 위해)
    #TODO LIST
    1. 채팅룸 종류 나누기
    2. (1 선행) 게임 룰 만들기
    3. (2 선행) 틱톡 허가 신청

---
+++ src/main/java/com/forestfull/config/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.forestfull.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.forestfull")
+public class DataSourceConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        factoryBean.setTypeAliasesPackage("com.forestfull");
+
+        // MyBatis 설정 객체 직접 설정
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+        factoryBean.setConfiguration(configuration);
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
